9b799de6b32048ca3991bb18bc3271c852cdb72f
TayiebK <150594024+mtk2608@users.noreply.github.com>
2024-04-05 11:41:03 +0200

    Classes Added
    - Logic needs to be adjusted slightly

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -1,25 +1,15 @@
-﻿namespace WeatherApp
+﻿using Newtonsoft.Json;
+
+namespace WeatherApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 
 }
---
+++ WeatherApp/Models/GPS.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    public class GPS
+    {
+        public async Task<string> GetCachedLocation()
+        {
+            try
+            {
+                Location location = await Geolocation.Default.GetLastKnownLocationAsync();
+
+                if (location != null)
+
+                    return $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                Console.WriteLine("Handle not supported on device exception");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                Console.WriteLine("Handle not enabled on device exception");
+            }
+            catch (PermissionException pEx)
+            {
+                Console.WriteLine("Handle permission exception");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to get location");
+            }
+            return "None";
+        }
+
+
+        private CancellationTokenSource _cancelTokenSource;
+        private bool _isCheckingLocation;
+
+        public async Task<Location> GetCurrentLocation()
+        {
+            Location location = null;
+            try
+            {
+                _isCheckingLocation = true;
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.High, TimeSpan.FromSeconds(10));
+
+                _cancelTokenSource = new CancellationTokenSource();
+
+                location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                if (location != null)
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _isCheckingLocation = false;
+            }
+            return location;
+
+        }
+
+        public void CancelRequest()
+        {
+            if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+                _cancelTokenSource.Cancel();
+        }
+    }
+}
