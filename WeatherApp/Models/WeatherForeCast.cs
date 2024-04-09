f336efa8150cc898c3ac1f3d5a148bade03ca27a
mtk2608 <kamaldientayieb@gmail.com>
2024-04-09 23:29:53 +0200

    Logic Fixed

--- WeatherApp/Models/WeatherForeCast.cs
+++ WeatherApp/Models/WeatherForeCast.cs
@@ -9,15 +9,16 @@ namespace WeatherApp.Models
 {
     public class WeatherForeCast
     {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
         public class Clouds
         {
             public int all { get; set; }
         }
 
-        public class Coord
+        public class CoOrd
         {
-            public double lon { get; set; }
-            public double lat { get; set; }
+            public double longitude { get; set; }
+            public double latitude { get; set; }
         }
 
         public class Main
@@ -38,9 +39,9 @@ namespace WeatherApp.Models
             public double _1h { get; set; }
         }
 
-        public class WeatherForecast
+        public class WeatherForecast //Object Orientation
         {
-            public Coord coord { get; set; }
+            public CoOrd coOrd { get; set; }
             public List<Weather> weather { get; set; }
             public string @base { get; set; }
             public Main main { get; set; }
